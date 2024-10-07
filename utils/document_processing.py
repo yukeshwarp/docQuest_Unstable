628101d105ffdfa2104f028483d7a2c9e93c809e
yukeshwarp <Yukeshwarp@docu3c.com>
2024-10-07 23:33:20 +0530

    Update document_processing.py

--- utils/document_processing.py
+++ utils/document_processing.py
@@ -17,7 +17,7 @@ def remove_stopwords_and_blanks(text):
     cleaned_text = ' '.join(word for word in text.split())
     return cleaned_text
 
-def detect_ocr_images_and_vector_graphics_in_pdf(pdf_document, ocr_text_threshold=0.19):
+def detect_ocr_images_and_vector_graphics_in_pdf(pdf_document, ocr_text_threshold=0.4):
     """Detect pages with OCR images or vector graphics."""
     detected_pages = []
 
@@ -116,7 +116,7 @@ def process_pdf_pages(uploaded_file):
     document_data = {"pages": [], "name": uploaded_file.name}
     previous_summary = ""
 
-    detected_images = detect_ocr_images_and_vector_graphics_in_pdf(pdf_document, 0.18)
+    detected_images = detect_ocr_images_and_vector_graphics_in_pdf(pdf_document, 0.4)
 
     for page_number in range(len(pdf_document)):
         page = pdf_document.load_page(page_number)
