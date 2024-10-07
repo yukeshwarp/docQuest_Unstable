ef16100b6c160a1dd3df1eab81e52d35b58844ab
yukeshwarp <Yukeshwarp@docu3c.com>
2024-10-07 23:19:07 +0530

    Update document_processing.py

--- utils/document_processing.py
+++ utils/document_processing.py
@@ -17,7 +17,7 @@ def remove_stopwords_and_blanks(text):
     cleaned_text = ' '.join(word for word in text.split())
     return cleaned_text
 
-def detect_ocr_images_and_vector_graphics_in_pdf(pdf_document, ocr_text_threshold=0.19):
+def detect_ocr_images_and_vector_graphics_in_pdf(pdf_document, ocr_text_threshold=0.4):
     """Detect pages with OCR images or vector graphics."""
     detected_pages = []
 
@@ -27,16 +27,21 @@ def detect_ocr_images_and_vector_graphics_in_pdf(pdf_document, ocr_text_threshol
         text = page.get_text("text")
         text_blocks = page.get_text("blocks")
         vector_graphics_detected = any(page.get_drawings())
+        page_area = page.rect.width * page.rect.height
+        text_area = sum((block[2] - block[0]) * (block[3] - block[1]) for block in text_blocks)
+        text_coverage = text_area / page_area
+        pix = page.get_pixmap() 
+        img_data = pix.tobytes("png")
+        base64_image = base64.b64encode(img_data).decode("utf-8")
 
-        if (images or vector_graphics_detected) and text.strip():
-            page_area = page.rect.width * page.rect.height
-            text_area = sum((block[2] - block[0]) * (block[3] - block[1]) for block in text_blocks)
-            text_coverage = text_area / page_area
+        if text_area==0:
+            detected_pages.append((page_number + 1, base64_image))
+            
+        elif (images or vector_graphics_detected) and text.strip():
+
 
             if text_coverage < ocr_text_threshold:
-                pix = page.get_pixmap() 
-                img_data = pix.tobytes("png")
-                base64_image = base64.b64encode(img_data).decode("utf-8")
+
                 detected_pages.append((page_number + 1, base64_image))
 
     return detected_pages
@@ -115,7 +120,7 @@ def process_pdf_pages(uploaded_file):
     document_data = {"pages": [], "name": uploaded_file.name}
     previous_summary = ""
 
-    detected_images = detect_ocr_images_and_vector_graphics_in_pdf(pdf_document, 0.19)
+    detected_images = detect_ocr_images_and_vector_graphics_in_pdf(pdf_document, 0.4)
 
     for page_number in range(len(pdf_document)):
         page = pdf_document.load_page(page_number)
